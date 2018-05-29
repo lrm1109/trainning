c9e219d9d890cab6aaaff17ad24ad428613d11d6
lrm1109 <ruomian.lin@dbjtech.com>
2018-05-29 11:57:10 +0800

    串行执行

--- trainning05/trainning05.js
+++ trainning05/trainning05.js
@@ -1,16 +1,14 @@
 const request = require('request')
 const rp = require('request-promise')
+const util = require('util')
 
 const MAX = 1000000
 const comparisonCallback = (minNum, maxNum, callback) => {
 	const num = Math.floor((minNum + maxNum) / 2)
 	console.log(num)
 	request.post(`http://localhost:8081/${num}`, (error, response, body) => {
-		if (error) {
-			console.log(error)
-		}
 		if (body === 'equal') {
-			callback(num)
+			callback(error, num)
 		} else if (body === 'bigger') {
 			comparisonCallback(minNum, num, callback)
 		} else if (body === 'smaller') {
@@ -18,7 +16,6 @@ const comparisonCallback = (minNum, maxNum, callback) => {
 		}
 	})
 }
-
 
 const comparePromise = (minNum, maxNum) => {
 	const number = Math.floor((minNum + maxNum) / 2)
@@ -36,36 +33,39 @@ const comparePromise = (minNum, maxNum) => {
 
 
 const comparisonAsync = async (minNum, maxNum) => {
-	try {
-		const number = Math.floor((minNum + maxNum) / 2)
-		console.log(number)
-		const options = { url: `http://localhost:8081/${number}`, method: 'POST' }
-		const response = await rp(options)
-		if (response === 'bigger') {
-			return await comparePromise(minNum, number)
-		} else if (response === 'smaller') {
-			return await comparePromise(number, maxNum)
-		}
-		return number
-	} catch (err) {
-		console.log(err)
+	const number = Math.floor((minNum + maxNum) / 2)
+	console.log(number)
+	const options = { url: `http://localhost:8081/${number}`, method: 'POST' }
+	const response = await rp(options)
+	if (response === 'bigger') {
+		return await comparePromise(minNum, number)
+	} else if (response === 'smaller') {
+		return await comparePromise(number, maxNum)
 	}
+	return number
 }
 
-async function play() {
+
+const play = async () => {
 	await rp('http://localhost:8081/start')
 
-	comparisonCallback(0, MAX, (num) => {
-		console.log(`callback: equal, guess ${num}`)
-	})
+	const callbackToPromise = util.promisify(comparisonCallback)
 
-	comparePromise(0, MAX).then((number) => {
+	try {
+		let num = await callbackToPromise(0, MAX)
+		console.log(`callback: equal, guess ${num}`)
+
+		num = await comparisonAsync(0, MAX)
+		console.log(`async: equal, guess ${num}`)
+	} catch (err) {
+		console.log('Error', err)
+	}
+
+	await comparePromise(0, MAX).then((number) => {
 		console.log(`promise: equal, guess ${number}`)
 	}).catch((err) => {
 		console.log(err)
 	})
-
-	console.log(`async: equal, guess ${await comparisonAsync(0, MAX)}`)
 }
 
 play()
